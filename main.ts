b76a1433741874dabeb0fcf2108ba3a694a4306d
Mark D. Lara <marklaraa@cox.net>
2017-11-10 06:27:10 -0800

    added new feature

---
+++ main.ts
@@ -0,0 +1,31 @@
+import {Observable} from "rxjs";
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string){
+    let xhr=new XMLHttpRequest();
+
+    xhr.addEventListener("load", ()=>{
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m=>{
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        })
+    });
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+click.subscribe(
+    e => load("movies.json"),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")   
+);
+
+
+
+
